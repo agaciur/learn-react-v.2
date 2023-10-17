766b032269582052002f381462cb6fe18b920a0c
Agata Rojewska <agata.rojewska@gmail.com>
2023-10-17 17:08:35 +0200

    Add a countdown timer to the end of the offer

--- src/ui/BestHotel.js
+++ src/ui/BestHotel.js
@@ -1,14 +1,33 @@
+import { useEffect, useState } from "react"
+import moment from "moment"
+
 const BestHotel = props => {
-    const hotel = props.getHotel()
-    if (!hotel) return null
+  const [time, setTime] = useState("")
+  const hotel = props.getHotel()
+  const endTime = moment().add(36, "minutes").add(45, "seconds")
+  let interval = null
+
+  useEffect(() => {
+    interval = setInterval(() => {
+      const leftTime = -moment().diff(endTime) / 1000
+      const minutes = Math.floor(leftTime / 60)
+      const seconds = Math.floor(leftTime % 60)
+      setTime(`minut: ${minutes} i sekund: ${seconds}`)
+    }, 1000)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
   return (
     <div className='container card bg-success text-white'>
       <div className='card-header'>Najlepsza oferta!</div>
       <div className='card-body'>
-      <div className="d-flex justify-content-between">
-      <h5 className='card-title'>{hotel.name}</h5>
-        <p>Ocena: {hotel.rating}</p>
-      </div>
+        <div className='d-flex justify-content-between'>
+          <h5 className='card-title'>{hotel.name}</h5>
+          <p>Ocena: {hotel.rating}</p>
+        </div>
+        <p>Do końca oferty pozostało: {time}</p>
         <a
           href='#'
           className='btn btn-sm btn-light'>
